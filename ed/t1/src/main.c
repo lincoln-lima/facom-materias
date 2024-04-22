03540a6442c407c1a1f3a9bf8436d3c3eee3f06b
Lincoln Cesar Gomes Lima <lincoln.lima@tre-ms.jus.br>
2024-04-22 18:18:39 -0400

    incluida biblioteca para parser de json

--- ed/t1/src/main.c
+++ ed/t1/src/main.c
@@ -1,24 +1,40 @@
 #include <stdio.h>
-#include <json-c/json.h>
+#include "../include/youtube_episode_jsense/jsense.h"
 
 typedef struct {
 	int cod_ibge;
-	char[] nome;
+	char * nome;
 	int latitude;
+	int longitude;
 	int capital;
 	int cod_uf;
 	int siafi_id;
 	int ddd;
-	char[] fuso;
+	char * fuso;
+} Municipio;
 
-} Municipios;
+void * aloca_municipio(int ibge, char * nome, int latitude, int longitude, int capital, int uf, int id, int ddd, char * fuso) {
+	Municipio * cidade = malloc(sizeof(Municipio));
+
+	//usar strpcy
+	cidade->cod_ibge = ibge;
+	cidade->nome = nome;
+	cidade->latitude = latitude;
+	cidade->longitude = longitude;
+	cidade->capital = capital;
+	cidade->cod_uf = uf;
+	cidade->siafi_id = id;
+	cidade->ddd = ddd
+	cidade->fuso = fuso;
+
+	return cidade;
+}
 
 int main() {
-	FILE * arq = fopen("../file/municipios.json", r);
+	JSENSE * arq = jse_from_file("./file/municipios.json");
 
-	if(!arq) printf("erro para abrir arquivo\n");
+	printf("%s\n", jse_get(arq, "[0].nome"));
+	jse_free(arq);
 
-	char buffer[2048];
-	
 	return EXIT_SUCCESS;
 }
